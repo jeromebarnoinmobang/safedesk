50f731eaa3020ec5f0cc63fff17956b78f0462ae
Jerome Barnoin <contact@mobang.fr>
2026-08-06 18:12:10 +0200

    chrome propre a chaque session RDP: startwm.sh tue l'instance fantome + purge les verrous Singleton au demarrage (evite le lancement invisible attache au bureau Selkies)

--- files/etc/xrdp/startwm.sh
+++ files/etc/xrdp/startwm.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 [ -r /etc/default/locale ] && . /etc/default/locale && export LANG LANGUAGE
 export XDG_CURRENT_DESKTOP=KDE XDG_SESSION_DESKTOP=KDE DESKTOP_SESSION=plasma
+# SafeDesk : repartir avec un Chrome propre a chaque session RDP. Sinon le lancement
+# de Chrome sur :10 s'accroche a l'instance de fond (bureau Selkies :1) et reste invisible.
+pkill -x chrome 2>/dev/null
+rm -f /config/.config/google-chrome/Singleton* /config/.config/chromium/Singleton* 2>/dev/null
 exec dbus-launch --exit-with-session startplasma-x11
